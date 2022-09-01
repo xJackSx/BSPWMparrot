e13d1984ba585f6ee6444c207c2439abfefec77b
Jack <jack@localhost.localdomain>
2022-09-01 18:10:51 -0500

    VMware

--- Config/bin/ethernet_status.sh
+++ Config/bin/ethernet_status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig wlp3s0 | grep "inet " | awk '{print $2}')%{u-}"
+echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}"
