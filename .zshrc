3b00abc6b9f4412fc47e6f8fdcc7bb93e033962b
Jack <jack@localhost.localdomain>
2022-09-05 14:09:17 +0000

    VMware

--- .zshrc
+++ .zshrc
@@ -66,6 +66,7 @@ alias catnl='batcat'
 
 # Plugins
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh-sudo/sudo.plugin.zsh
 
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev
 
 # Instalando Requerimientos para la polybar
 
-sudo apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libnl-genl-3-dev
 
 # Dependencias de Picom
 
