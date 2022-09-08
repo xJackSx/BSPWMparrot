1d5d8e860939f05b30484321410e51fa00c1cf97
Jack <jack@localhost.localdomain>
2022-09-08 14:07:09 -0500

    VMparrot

--- Config/bin/htb_target.sh
+++ Config/bin/htb_target.sh
@@ -4,13 +4,10 @@ ip_target=$(cat ~/.config/bin/target | awk '{print $1}')
 name_target=$(cat ~/.config/bin/target | awk '{print $2}')
 
 	if [ $ip_target ] && [ $name_target ]; then
-
-		echo "%{F#e51d0b}什%{F#ffffff} $ip_target - $name_target"
-
+	echo "%{F#e51d0b}什%{F#ffffff} $ip_target - $name_target"
 	elif [ $(cat ~/.config/bin/target | wc -w) -eq 1 ]; then
-
-		echo "%{F#e51d0b}什%{F#ffffff} $ip_target"
+	echo "%{F#e51d0b}什%{F#ffffff} $ip_target"
 	else
-    		echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} No target"
+	echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} No target"
 	fi
 
